6d7445980fcfffe355f139ee7608e9f01ba5319f
CMinder <minderctrl@gmail.com>
2024-05-09 16:32:26 +0800

    没有考虑最后走到的时候所有箭靶都要是0 并且没考虑当前位置走不通的时候箭靶加回来

--- src/path_mystery/Main.java
+++ src/path_mystery/Main.java
@@ -3,41 +3,56 @@ package path_mystery;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Statement;
 import java.util.Stack;
 
 public class Main {
+    //检查所有箭靶是否全部用完
+    public static boolean check(int n, int[] row, int[] col) {
+        for (int i = 0; i < n; i++) {
+            if (row[i] != 0 || col[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     //使用dfs遍历地图
-    public static boolean dfs(int n, int[] row, int[] col,
+    public static boolean dfs(int n, int[][] map, int[] row, int[] col,
                               int i, int j, Stack<Integer> stack) {
         //判断上一步走的是否合理 越界
         if (i < 0 || i >= n || j < 0 || j >= n) {
             return false;
         }
-        if (row[j] < 1 || col[i] < 1) {//或者 箭靶数目小于1 上一步走的不合理
+        if (row[j] < 1 || col[i] < 1 || map[i][j] == 1) {//走过了 或者 箭靶数目小于1 上一步走的不合理
             return false;
         }
-        //上一步移动到当前位置 当前位置箭靶减1
         row[j]--;
         col[i]--;
-        if (i == n - 1 && j == n - 1) {//走到了右下角
+        map[i][j] = 1;//标记为已经走过
+        //上一步移动到当前位置 当前位置箭靶减1
+        if (i == n - 1 && j == n - 1 && check(n, row, col)) {//走到了右下角
             stack.push(i * n + j);//将最后一个位置加入
             return true;
         }
         //尝试往四个方向走
         //记录从当前位置是否能到右下角
-        boolean flag = dfs(n, row, col, i - 1, j, stack);//上
+        boolean flag = dfs(n, map, row, col, i - 1, j, stack);//上
         if (!flag) {
-            flag = dfs(n, row, col, i + 1, j, stack);//下
+            flag = dfs(n, map, row, col, i + 1, j, stack);//下
         }
         if (!flag) {
-            flag = dfs(n, row, col, i, j - 1, stack);//左
+            flag = dfs(n, map, row, col, i, j - 1, stack);//左
         }
         if (!flag) {
-            flag = dfs(n, row, col, i, j + 1, stack);//右
+            flag = dfs(n, map, row, col, i, j + 1, stack);//右
         }
         if (flag) {//如果从当前位置能够到达右下角
             stack.push(i * n + j);//将当前位置加入栈
+        } else {
+            //从当前位置走不通 加回来
+            row[j]++;
+            col[i]++;
+            map[i][j] = 0;//标记为未访问
         }
         return flag;
     }
@@ -62,7 +77,7 @@ public class Main {
 
             //处理 dfs 使用stack记录路径
             Stack<Integer> path = new Stack<>();
-            dfs(n, row, col, 0, 0, path);
+            dfs(n, new int[n][n], row, col, 0, 0, path);
             while (!path.isEmpty()) {
                 Integer res = path.pop();
                 System.out.print(res + " ");
