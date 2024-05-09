a03a3c0fc40a78b48a8c4d84cf32e7891f2dcdfe
CMinder <minderctrl@gmail.com>
2024-05-09 16:13:49 +0800

    进行了dfs的调整和实现

--- src/path_mystery/Main.java
+++ src/path_mystery/Main.java
@@ -3,11 +3,43 @@ package path_mystery;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Statement;
+import java.util.Stack;
 
 public class Main {
     //使用dfs遍历地图
-    public static void dfs(int[][] map, int[] row, int[] col) {
-
+    public static boolean dfs(int n, int[] row, int[] col,
+                              int i, int j, Stack<Integer> stack) {
+        //判断上一步走的是否合理 越界
+        if (i < 0 || i >= n || j < 0 || j >= n) {
+            return false;
+        }
+        if (row[j] < 1 || col[i] < 1) {//或者 箭靶数目小于1 上一步走的不合理
+            return false;
+        }
+        //上一步移动到当前位置 当前位置箭靶减1
+        row[j]--;
+        col[i]--;
+        if (i == n - 1 && j == n - 1) {//走到了右下角
+            stack.push(i * n + j);//将最后一个位置加入
+            return true;
+        }
+        //尝试往四个方向走
+        //记录从当前位置是否能到右下角
+        boolean flag = dfs(n, row, col, i - 1, j, stack);//上
+        if (!flag) {
+            flag = dfs(n, row, col, i + 1, j, stack);//下
+        }
+        if (!flag) {
+            flag = dfs(n, row, col, i, j - 1, stack);//左
+        }
+        if (!flag) {
+            flag = dfs(n, row, col, i, j + 1, stack);//右
+        }
+        if (flag) {//如果从当前位置能够到达右下角
+            stack.push(i * n + j);//将当前位置加入栈
+        }
+        return flag;
     }
 
 
@@ -28,8 +60,13 @@ public class Main {
                 col[i] = Integer.parseInt(split[i]);
             }
 
-            //处理 dfs
-            int[][] map = new int[n][n];//地图信息 1表示路径
+            //处理 dfs 使用stack记录路径
+            Stack<Integer> path = new Stack<>();
+            dfs(n, row, col, 0, 0, path);
+            while (!path.isEmpty()) {
+                Integer res = path.pop();
+                System.out.print(res + " ");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
