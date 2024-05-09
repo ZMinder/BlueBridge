92a22f803ce26f4712f9b3b3ec086285ddd48328
CMinder <minderctrl@gmail.com>
2024-05-09 17:54:41 +0800

    基本完成solve方法

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,10 +3,38 @@ package game;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main {
-    public static final int MAX = 100005;
-    public static int[] nums = new int[MAX];//存储n个数
+    //求窗口最大值和最小值 分别计算累加和 相减*(n-k+1)即为结果
+    public static double solve(int n, int k, int[] nums, int[] max, int[] min) {
+        LinkedList<Integer> maxList = new LinkedList<>();//存储最大的
+        LinkedList<Integer> minList = new LinkedList<>();//存储最小的
+        long minSum = 0;
+        long maxSum = 0;
+        for (int right = 0; right < n; right++) {
+            if (!maxList.isEmpty() && right - maxList.peekFirst() == k) {//窗口内达到了k个数
+                maxList.pollFirst();//将最大值pop出来
+            }
+            if (!minList.isEmpty() && right - minList.peekFirst() == k) {//窗口内达到了k个数
+                minList.pollFirst();//将最大值pop出来
+            }
+            int cur = nums[right];//当前要加入窗口的数
+            while (!maxList.isEmpty() && nums[maxList.peekLast()] <= cur) {//将小于等于cur的pop出来
+                maxList.pollLast();
+            }
+            while (!minList.isEmpty() && nums[minList.peekLast()] >= cur) {//将大于等于cur的pop出来
+                minList.pollLast();
+            }
+            maxList.addLast(right);//将cur加入窗口
+            minList.addLast(right);
+            max[right - k + 1] = maxList.peekFirst();//决定最大值
+            min[right - k + 1] = minList.peekFirst();//决定最小值
+            minSum += minList.peekFirst();
+            maxSum += maxList.peekFirst();
+        }
+        return (maxSum - minSum) / (double) (n - k + 1);
+    }
 
     public static void main(String[] args) {
         BufferedReader br = null;
@@ -16,9 +44,12 @@ public class Main {
             int n = Integer.parseInt(split[0]);//n个数
             int k = Integer.parseInt(split[1]);//窗口大小
             split = br.readLine().split(" ");
+            int[] nums = new int[n];//存储n个数
             for (int i = 0; i < n; i++) {//读取数据
                 nums[i] = Integer.parseInt(split[i]);
             }
+            int[] max = new int[n - k + 1];//窗口最大值
+            int[] min = new int[n - k + 1];//窗口最小值
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
